e665b4442936fd223d55a25e0eee775bd8dda68d
octofoxx <devilmaycry87x@gmail.com>
2023-05-27 18:02:50 -0700

    added in logic to pull value of description from local storage

--- script.js
+++ script.js
@@ -44,6 +44,16 @@ setInterval(newHour, 1000);
   /*localStorage.getItem(based on time id)
   .text("insert pulled from storage here, possible a key from object") */
 
+  $("#9 .description").val(localStorage.getItem("9"));
+  $("#10 .description").val(localStorage.getItem("10"));
+  $("#11 .description").val(localStorage.getItem("11"));
+  $("#12 .description").val(localStorage.getItem("12"));
+  $("#13 .description").val(localStorage.getItem("13"));
+  $("#14 .description").val(localStorage.getItem("14"));
+  $("#15 .description").val(localStorage.getItem("15"));
+  $("#16 .description").val(localStorage.getItem("16"));
+  $("#17 .description").val(localStorage.getItem("17"));
+
   var today = dayjs();
   $("#currentDay").text(today.format("dddd MMMM DD"))
 });
